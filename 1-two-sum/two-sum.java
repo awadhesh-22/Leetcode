8ea4aa2e80d51dea96a4a6073ee7055546334033
awadhesh-22 <117575978+awadhesh-22@users.noreply.github.com>
2025-02-09 14:30:43 +0530

    Time: 2 ms (98.90%) | Memory: 44.8 MB (87.33%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,19 +1,13 @@
-import java.util.HashMap;
-
 class Solution {
-  public static int[] twoSum(int[] nums, int target) {
-      // 1. Iterate over every possible number pair
-      for (int i = 0; i < nums.length; i++) {
-          // j is always ahead of i so that we don't re-evaluate already evaluated sums
-          for (int j = i + 1; j < nums.length; j++) {
-              // 2. Check if a given pair adds up to our target
-              if (nums[i] + nums[j] == target) {
-                  // Return the indices when a pair has been found
-                  return new int[]{i, j};
-              }
-          }
-      }
-      // Return an empty array if no pair is found
-      return new int[]{};
-  }
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> d = new HashMap<>();
+        for (int i = 0;; ++i) {
+            int x = nums[i];
+            int y = target - x;
+            if (d.containsKey(y)) {
+                return new int[] {d.get(y), i};
+            }
+            d.put(x, i);
+        }
+    }
 }
